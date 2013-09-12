42af13c46a928fb940f0ffff558a986393cf6324
Arie <arie@wego.com>
2013-09-12 20:57:21 +0700

    Initial Commit

---
+++ Suara Hewan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Suara Hewan
+//
+//  Created by Arie on 9/12/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Suara_HewanAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([Suara_HewanAppDelegate class]));
+	}
+}
